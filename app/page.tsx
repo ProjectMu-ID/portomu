caef30db1d8acb840bd338a7bb713f291fac5ef7
Qiboi <iqiar123@gmail.com>
2026-01-01 14:21:39 +0700

    update root page (hidden pricing section)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from '@/components/sections/Hero'
 import About from '@/components/sections/About'
 import ProjectGrid from '@/components/sections/ProjectGrid'
-import PricingSection from '@/components/sections/PricingSection'
+// import PricingSection from '@/components/sections/PricingSection'
 
 export default function Home() {
   return (
